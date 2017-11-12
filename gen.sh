e4d4950a4362d5b93c6843027473ac5224ac37d4
leavehouse <heavelouse@gmail.com>
2017-11-12 13:28:16 -0600

    Make gen operate on a directory of files

--- gen.sh
+++ gen.sh
@@ -1,5 +1,11 @@
+# Input is a path to a directory of markdown files
+INPUT_DIR=$1
 OUTPUT_DIR=output
-FILE_NAME=$(basename $1 | sed 's/\..*//')
-OUTPUT_FILE=$OUTPUT_DIR/$FILE_NAME.html
-echo "Generating output file $OUTPUT_FILE..."
-pandoc $1 --standalone -f markdown+smart+yaml_metadata_block -t html5 -o $OUTPUT_FILE
+mkdir -p $OUTPUT_DIR
+for INPUT_FILE in `ls $INPUT_DIR/*.md`
+do
+    OUTPUT_FILE_NAME=$(basename $INPUT_FILE | sed 's/\..*/.html/')
+    OUTPUT_FILE=$OUTPUT_DIR/$OUTPUT_FILE_NAME
+    echo "Generating output file $OUTPUT_FILE ..."
+    pandoc $INPUT_FILE --standalone -f markdown+smart+yaml_metadata_block -t html5 -o $OUTPUT_FILE
+done
